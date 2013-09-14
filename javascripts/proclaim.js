315b1e672ffcfdb65c557c1363b5765d03e4fae1
bradley <bradley.j.griffith@gmail.com>
2013-09-13 20:30:00 -0500

    project initiation

---
+++ javascripts/proclaim.js
@@ -0,0 +1,67 @@
+$(window).load(function(){
+
+	;(function(_modals, $) {
+		// Class constructor
+		var Class = function() {
+      return function(params) {
+        if ( !(this instanceof arguments.callee) ) {
+          return new arguments.callee(arguments);
+        }
+        this.initialize.apply(this,(params.callee ? params : arguments));
+      };
+    };
+	    
+		var ProclaimModal = Class();
+    ProclaimModal.prototype = {
+      initialize: function(_modal) {
+      	this.modal = $(_modal);
+      	this.should_reset = false;
+      	this.modal.wrap('<div class="modal-background" />');
+      	this.modal_bg = this.modal.parent('.modal-background');
+
+      	this.setupListeners();
+      },
+      setupListeners: function() {
+      	var self = this;
+		    this.modal.click(function(e) { e.stopPropagation(); });
+		    this.modal_bg.click(function(e) { self.conceal(); });
+		    // Listens for end of CSS animations.
+		    this.modal.on("webkitAnimationEnd oAnimationEnd oanimationend animationend msTransitionEnd", function() {
+		        self.toggleModalClasses();
+		    });
+      },
+      proclaim: function() {
+        this.modal_bg.show(); // CSS animation begins.
+      },
+      conceal: function() {
+      	this.should_reset = true;
+      	this.modal_bg.addClass('fadeOut'); // CSS animation begins.
+      },
+      toggleModalClasses: function() {
+      	if (this.should_reset) {
+          this.modal_bg.hide().removeClass('fadeOut');
+          this.should_reset = false;
+        }
+      }
+    }
+
+    _modals.each(function(ix, o){
+      $(o).data('proclaim.instance', new ProclaimModal(o));
+    });
+
+    $.fn.proclaim = function() {
+      this.each(function() {
+        $(this).data('proclaim.instance').proclaim();
+      });
+    }
+    
+    $.fn.conceal = function() {
+      this.each(function() {
+        $(this).data('proclaim.instance').conceal();
+      });
+    }
+	    
+	  return _modals;
+	    
+	})($('.proclaim-modal'), jQuery);
+});
